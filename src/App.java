be7d84ea6accaad239d343430c404b06d58afd6b
swiftwombat <61484083+swiftwombat@users.noreply.github.com>
2023-11-12 14:45:34 +1100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,37 @@
+/**
+ * Access point for the application.
+ *
+ * TODO: Ensure date is updated after project is tempalted.
+ * @author Zachary Cockshutt
+ * @since  2023-08-01
+ */
+public class App
+{
+    public App()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        var me = new App();
+        me.init();
+        me.run();
+    }
+
+    /**
+     * Handles app initialization.
+     */
+    private void init()
+    {
+
+    }
+
+    /**
+     * Handles the runtime logic.
+     */
+    private void run()
+    {
+        System.out.println("Hello World!");
+    }
+}
